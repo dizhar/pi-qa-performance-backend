db168e2053c23750f2be377914f7ba55a6b8e1a0
FIRST_NAME LAST_NAME <dizhar@akamai.com>
2020-08-26 16:07:09 +0300

    add create session folder if not exist

--- src/support/create.ts
+++ src/support/create.ts
@@ -8,9 +8,18 @@ public async setSessionConfigFile(uniqueId: string) {
         "sessionId": uniqueId
       }
 
+     await craeteDir("sessions");
     fs.writeFileSync('./sessions/config-session.json', JSON.stringify(config));
 }
 
 
 
+}
+
+async function  craeteDir(dirname: string) {
+    var dir = `./${dirname}`;
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
 }
