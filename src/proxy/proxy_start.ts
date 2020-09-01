921a0f87f09f54f7e37837d1c4d09adbd89da6f9
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-09-01 12:15:10 +0300

    Changed paths in proxy/proxy_start.ts

--- src/proxy/proxy_start.ts
+++ src/proxy/proxy_start.ts
@@ -1,13 +1,12 @@
 
 require('./proxy');
 
-let obj: string = require('../../config/config-session.json');
+// let obj: string = require('../../config/config-session.json')
+// let config = require(`../../config/temp-proxy_${obj['sessionId']}.json`);
 
-let config = require(`../../config/temp-proxy_${obj['sessionId']}.json`);
+let obj: string = require('./config/config-session.json')
+let config = require(`./config/temp-proxy_${obj['sessionId']}.json`);
 
 module.exports = async function(context, commands) {
-
         context.proxy  =  await "<head>[0]"['proxyReplace'](`<head><link rel="dns-prefetch" href="${config['script_tag']}" ><script src="${config['script_tag']}"></script>`, config['port'])
- 
 };
-
